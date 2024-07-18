94cdf2ba6844cfac134f5ac06af2cb9823963ada
Samir <sharipovsamir480@gmail.com>
2024-07-18 06:23:00 +0500

    18.07.2024 2

--- src/main/java/uz/pdp/springsecurity/service/ProductService.java
+++ src/main/java/uz/pdp/springsecurity/service/ProductService.java
@@ -186,7 +186,7 @@ public class ProductService {
         product.setShippingClass(productDto.getShippingClass());
         product.setAttributes(productDto.getAttributes());
         product.setSoldIndividualy(productDto.getSoldIndividualy());
-        product.setIsGlobal(productDto.isGlobal());
+        product.setIsGlobal(productDto.getGlobal());
 
 
         if (productDto.getCategoryId() != null) {
