e98adb5888f246d140b9cbca9ef121c88c5171e2
Samir <sharipovsamir480@gmail.com>
2025-01-09 17:56:40 +0500

    fa

---
+++ target/generated-sources/annotations/uz/pdp/springsecurity/mapper/AddressMapperImpl.java
@@ -0,0 +1,90 @@
+package uz.pdp.springsecurity.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.pdp.springsecurity.entity.Address;
+import uz.pdp.springsecurity.payload.AddressDto;
+import uz.pdp.springsecurity.payload.AddressGetDto;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-09T14:24:57+0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class AddressMapperImpl implements AddressMapper {
+
+    @Override
+    public AddressGetDto toDto(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+
+        AddressGetDto addressGetDto = new AddressGetDto();
+
+        addressGetDto.setParentId( addressParentAddressId( address ) );
+        addressGetDto.setParentName( addressParentAddressName( address ) );
+        addressGetDto.setId( address.getId() );
+        addressGetDto.setName( address.getName() );
+
+        return addressGetDto;
+    }
+
+    @Override
+    public List<AddressGetDto> toDtoList(List<Address> addresses) {
+        if ( addresses == null ) {
+            return null;
+        }
+
+        List<AddressGetDto> list = new ArrayList<AddressGetDto>( addresses.size() );
+        for ( Address address : addresses ) {
+            list.add( toDto( address ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateEntityFromDto(AddressDto addressDto, Address address) {
+        if ( addressDto == null ) {
+            return;
+        }
+
+        if ( addressDto.getName() != null ) {
+            address.setName( addressDto.getName() );
+        }
+    }
+
+    private UUID addressParentAddressId(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        Address parentAddress = address.getParentAddress();
+        if ( parentAddress == null ) {
+            return null;
+        }
+        UUID id = parentAddress.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+
+    private String addressParentAddressName(Address address) {
+        if ( address == null ) {
+            return null;
+        }
+        Address parentAddress = address.getParentAddress();
+        if ( parentAddress == null ) {
+            return null;
+        }
+        String name = parentAddress.getName();
+        if ( name == null ) {
+            return null;
+        }
+        return name;
+    }
+}
---
+++ target/generated-sources/annotations/uz/pdp/springsecurity/mapper/BalanceMapperImpl.java
@@ -0,0 +1,109 @@
+package uz.pdp.springsecurity.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.pdp.springsecurity.entity.Balance;
+import uz.pdp.springsecurity.entity.BalanceHistory;
+import uz.pdp.springsecurity.entity.PaymentMethod;
+import uz.pdp.springsecurity.payload.BalanceHistoryDto;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-09T14:24:57+0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class BalanceMapperImpl implements BalanceMapper {
+
+    @Override
+    public BalanceHistoryDto toDto(BalanceHistory balanceHistory) {
+        if ( balanceHistory == null ) {
+            return null;
+        }
+
+        BalanceHistoryDto balanceHistoryDto = new BalanceHistoryDto();
+
+        balanceHistoryDto.setPayMethodType( balanceHistoryBalancePaymentMethodType( balanceHistory ) );
+        balanceHistoryDto.setPayMethodId( balanceHistoryBalancePaymentMethodId( balanceHistory ) );
+        balanceHistoryDto.setBalanceId( balanceHistoryBalanceId( balanceHistory ) );
+        balanceHistoryDto.setDate( balanceHistory.getCreatedAt() );
+        balanceHistoryDto.setSumma( balanceHistory.getSumma() );
+        balanceHistoryDto.setPlus( balanceHistory.isPlus() );
+        balanceHistoryDto.setAccountSumma( balanceHistory.getAccountSumma() );
+        balanceHistoryDto.setTotalSumma( balanceHistory.getTotalSumma() );
+        balanceHistoryDto.setCurrency( balanceHistory.getCurrency() );
+        balanceHistoryDto.setDescription( balanceHistory.getDescription() );
+
+        return balanceHistoryDto;
+    }
+
+    @Override
+    public List<BalanceHistoryDto> toDto(List<BalanceHistory> balanceHistoryList) {
+        if ( balanceHistoryList == null ) {
+            return null;
+        }
+
+        List<BalanceHistoryDto> list = new ArrayList<BalanceHistoryDto>( balanceHistoryList.size() );
+        for ( BalanceHistory balanceHistory : balanceHistoryList ) {
+            list.add( toDto( balanceHistory ) );
+        }
+
+        return list;
+    }
+
+    private String balanceHistoryBalancePaymentMethodType(BalanceHistory balanceHistory) {
+        if ( balanceHistory == null ) {
+            return null;
+        }
+        Balance balance = balanceHistory.getBalance();
+        if ( balance == null ) {
+            return null;
+        }
+        PaymentMethod paymentMethod = balance.getPaymentMethod();
+        if ( paymentMethod == null ) {
+            return null;
+        }
+        String type = paymentMethod.getType();
+        if ( type == null ) {
+            return null;
+        }
+        return type;
+    }
+
+    private UUID balanceHistoryBalancePaymentMethodId(BalanceHistory balanceHistory) {
+        if ( balanceHistory == null ) {
+            return null;
+        }
+        Balance balance = balanceHistory.getBalance();
+        if ( balance == null ) {
+            return null;
+        }
+        PaymentMethod paymentMethod = balance.getPaymentMethod();
+        if ( paymentMethod == null ) {
+            return null;
+        }
+        UUID id = paymentMethod.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+
+    private UUID balanceHistoryBalanceId(BalanceHistory balanceHistory) {
+        if ( balanceHistory == null ) {
+            return null;
+        }
+        Balance balance = balanceHistory.getBalance();
+        if ( balance == null ) {
+            return null;
+        }
+        UUID id = balance.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+}
