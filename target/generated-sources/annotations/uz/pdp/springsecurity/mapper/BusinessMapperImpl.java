5b4625242bc45f2369d4eb299f465f57cefec809
Samir <sharipovsamir480@gmail.com>
2025-01-16 15:41:48 +0500

    fa

---
+++ target/generated-sources/annotations/uz/pdp/springsecurity/mapper/BusinessMapperImpl.java
@@ -0,0 +1,88 @@
+package uz.pdp.springsecurity.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.pdp.springsecurity.entity.Business;
+import uz.pdp.springsecurity.payload.BusinessEditDto;
+import uz.pdp.springsecurity.payload.BusinessGetDto;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-15T18:13:49+0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class BusinessMapperImpl implements BusinessMapper {
+
+    @Override
+    public BusinessEditDto toGetOneDto(Business business) {
+        if ( business == null ) {
+            return null;
+        }
+
+        BusinessEditDto businessEditDto = new BusinessEditDto();
+
+        businessEditDto.setId( business.getId() );
+        businessEditDto.setName( business.getName() );
+        businessEditDto.setDescription( business.getDescription() );
+        businessEditDto.setActive( business.isActive() );
+        businessEditDto.setDeleted( business.isDeleted() );
+        businessEditDto.setBusinessNumber( business.getBusinessNumber() );
+        businessEditDto.setStatus( business.getStatus() );
+        businessEditDto.setContractStartDate( business.getContractStartDate() );
+        businessEditDto.setContractEndDate( business.getContractEndDate() );
+
+        return businessEditDto;
+    }
+
+    @Override
+    public List<BusinessEditDto> toGetDtoList(List<Business> businesses) {
+        if ( businesses == null ) {
+            return null;
+        }
+
+        List<BusinessEditDto> list = new ArrayList<BusinessEditDto>( businesses.size() );
+        for ( Business business : businesses ) {
+            list.add( toGetOneDto( business ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public BusinessGetDto toDto(Business business) {
+        if ( business == null ) {
+            return null;
+        }
+
+        BusinessGetDto businessGetDto = new BusinessGetDto();
+
+        businessGetDto.setCreatedAt( business.getCreatedAt() );
+        businessGetDto.setUpdateAt( business.getUpdateAt() );
+        businessGetDto.setId( business.getId() );
+        businessGetDto.setName( business.getName() );
+        businessGetDto.setDescription( business.getDescription() );
+        businessGetDto.setActive( business.isActive() );
+        businessGetDto.setDeleted( business.isDeleted() );
+        businessGetDto.setContractStartDate( business.getContractStartDate() );
+        businessGetDto.setContractEndDate( business.getContractEndDate() );
+
+        return businessGetDto;
+    }
+
+    @Override
+    public List<BusinessGetDto> toDtoList(List<Business> business) {
+        if ( business == null ) {
+            return null;
+        }
+
+        List<BusinessGetDto> list = new ArrayList<BusinessGetDto>( business.size() );
+        for ( Business business1 : business ) {
+            list.add( toDto( business1 ) );
+        }
+
+        return list;
+    }
+}
---
+++ target/generated-sources/annotations/uz/pdp/springsecurity/mapper/ProductMapperImpl.java
@@ -0,0 +1,75 @@
+package uz.pdp.springsecurity.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import uz.pdp.springsecurity.entity.Category;
+import uz.pdp.springsecurity.entity.Product;
+import uz.pdp.springsecurity.payload.ProductGetForPurchaseDto;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-01-15T18:13:49+0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.13 (Amazon.com Inc.)"
+)
+@Component
+public class ProductMapperImpl implements ProductMapper {
+
+    @Override
+    public ProductGetForPurchaseDto toProductGetForPurchaseDto(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+
+        ProductGetForPurchaseDto productGetForPurchaseDto = new ProductGetForPurchaseDto();
+
+        productGetForPurchaseDto.setProductId( product.getId() );
+        productGetForPurchaseDto.setCategoryId( productCategoryId( product ) );
+        productGetForPurchaseDto.setName( product.getName() );
+        productGetForPurchaseDto.setBarcode( product.getBarcode() );
+        productGetForPurchaseDto.setBuyPrice( product.getBuyPrice() );
+        productGetForPurchaseDto.setSalePrice( product.getSalePrice() );
+        productGetForPurchaseDto.setBuyPriceDollar( product.getBuyPriceDollar() );
+        productGetForPurchaseDto.setBuyDollar( product.isBuyDollar() );
+        productGetForPurchaseDto.setSalePriceDollar( product.getSalePriceDollar() );
+        productGetForPurchaseDto.setSaleDollar( product.isSaleDollar() );
+        productGetForPurchaseDto.setGrossPrice( product.getGrossPrice() );
+        productGetForPurchaseDto.setGrossPriceDollar( product.getGrossPriceDollar() );
+        productGetForPurchaseDto.setGrossPriceMyControl( product.getGrossPriceMyControl() );
+        productGetForPurchaseDto.setProfitPercent( product.getProfitPercent() );
+        productGetForPurchaseDto.setMinQuantity( product.getMinQuantity() );
+
+        return productGetForPurchaseDto;
+    }
+
+    @Override
+    public List<ProductGetForPurchaseDto> toProductGetForPurchaseDtoList(List<Product> products) {
+        if ( products == null ) {
+            return null;
+        }
+
+        List<ProductGetForPurchaseDto> list = new ArrayList<ProductGetForPurchaseDto>( products.size() );
+        for ( Product product : products ) {
+            list.add( toProductGetForPurchaseDto( product ) );
+        }
+
+        return list;
+    }
+
+    private UUID productCategoryId(Product product) {
+        if ( product == null ) {
+            return null;
+        }
+        Category category = product.getCategory();
+        if ( category == null ) {
+            return null;
+        }
+        UUID id = category.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+}
