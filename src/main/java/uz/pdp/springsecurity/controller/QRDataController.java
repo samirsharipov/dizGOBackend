e7f2b5d9194562c79008f288275b31cd1d8c47f8
Samir <sharipovsamir480@gmail.com>
2025-02-24 16:35:04 +0500

    qr data done

--- src/main/java/uz/pdp/springsecurity/controller/QRDataController.java
+++ src/main/java/uz/pdp/springsecurity/controller/QRDataController.java
@@ -18,31 +18,26 @@ public class QRDataController {
     private final QRDataService qrDataService;
     private final ResponseEntityHelper responseEntityHelper;
 
-    @CheckPermission("ADD_QRDATA")
     @PostMapping
     public HttpEntity<?> add(@Valid @RequestBody QRDataDto qrDataDto) {
         return responseEntityHelper.buildResponse(qrDataService.add(qrDataDto));
     }
 
-    @CheckPermission("EDIT_QRDATA")
     @PutMapping("/{id}")
     public HttpEntity<?> edit(@PathVariable Long id, @RequestBody QRDataDto qrDataDto) {
         return responseEntityHelper.buildResponse(qrDataService.edit(id, qrDataDto));
     }
 
-    @CheckPermission("VIEW_QRDATA")
     @GetMapping("/{id}")
     public HttpEntity<?> get(@PathVariable Long id) {
         return responseEntityHelper.buildResponse(qrDataService.get(id));
     }
 
-    @CheckPermission("VIEW_QRDATA")
     @GetMapping
     public HttpEntity<?> getAll() {
         return responseEntityHelper.buildResponse(qrDataService.getAll());
     }
 
-    @CheckPermission("DELETE_QRDATA")
     @DeleteMapping("/{id}")
     public HttpEntity<?> delete(@PathVariable Long id) {
         return responseEntityHelper.buildResponse(qrDataService.delete(id));
