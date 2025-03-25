95b645429ebdca534b8ca13d3fd697909cdc045d
Samir <sharipovsamir480@gmail.com>
2025-03-25 11:46:46 +0500

    Merge pull request #199 from samirsharipov/samir
    clean mappers

--- src/main/java/uz/dizgo/erp/mapper/ProductTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package uz.dizgo.erp.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ProductTypeMapper {
-}
--- src/main/java/uz/dizgo/erp/mapper/ProductsReportMapper.java
+++
@@ -1,6 +0,0 @@
-package uz.dizgo.erp.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ProductsReportMapper {}
