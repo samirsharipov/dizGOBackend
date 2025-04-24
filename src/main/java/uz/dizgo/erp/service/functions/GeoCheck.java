0dc6c6809af2f231520bf4ad619f134a4b2f896a
Samir <sharipovsamir480@gmail.com>
2025-04-24 13:26:36 +0500

    fix: product ni barcode boyicha olish

--- src/main/java/uz/dizgo/erp/service/functions/GeoCheck.java
+++ src/main/java/uz/dizgo/erp/service/functions/GeoCheck.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Service;
 public class GeoCheck {
 
     public double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        final double R = 6371000; // Radius of the Earth in meters
+        final double R = 6371000;
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
                 * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return R * c; // Returns distance in meters
+        return R * c;
     }
 
     public boolean checkDistance(double distance, double radius) {
